327e6fe6b6e2d12bc6ac29c5620a2e894e9ea966
jufegare000 <juanfgallo@experimentality.co>
2021-09-13 10:27:52 -0500

    first commit

---
+++ src/main/java/co/edu/udea/pexshop/PexshopApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.pexshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PexshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PexshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/pexshop/PexshopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.pexshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PexshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
