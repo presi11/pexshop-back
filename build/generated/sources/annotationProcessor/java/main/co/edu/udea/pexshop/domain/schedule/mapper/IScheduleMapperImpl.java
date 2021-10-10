74b610e21181573664dfa56b6405081a39cc7891
presi11 <juan.ospina19@udea.edu.co>
2021-10-10 18:10:46 -0500

    add authorities

--- build/generated/sources/annotationProcessor/java/main/co/edu/udea/pexshop/domain/schedule/mapper/IScheduleMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/co/edu/udea/pexshop/domain/schedule/mapper/IScheduleMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-10-10T16:28:55-0500",
+    date = "2021-10-10T18:07:51-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 16.0.2 (Amazon.com Inc.)"
 )
 @Component
